78649aaf2e777ad99b49e6ff776757526197e550
kevinpauljose <bkevin1999@gmail.com>
2024-03-06 21:00:57 +0530

    Added cors

--- app.js
+++ app.js
@@ -18,9 +18,13 @@ var app = express();
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
-
+const corsOptions = {
+  AccessControlAllowOrigin: '*',  
+  origin: '*',  
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' 
+}
+app.use(cors(corsOptions))
 app.use(logger('dev'));
-app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
