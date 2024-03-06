b1759a263f78b8eb8fe1ef974fed2f49fd64a6cb
kevinpauljose <bkevin1999@gmail.com>
2024-03-06 20:19:00 +0530

    removed some configs

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ const connectDB = require('./db/db');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 connectDB();
