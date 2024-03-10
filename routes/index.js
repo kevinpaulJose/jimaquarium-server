bba5d82901db2bd26807933f2f9e94bd106e98c1
kevinpauljose <bkevin1999@gmail.com>
2024-03-10 19:15:06 +0530

    Added heart beat

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send({status: "working"})
+});
+router.get('/pulse', function(req, res, next) {
+  res.status(200).send({status: "working"})
+});
+
+router.get('/runPulse', function(req, res, next) {
+  let intervalId = null; // This will hold the reference to the interval
+  let count = 0;
+// Function to start the interval
+function startInterval() {
+  function repeatFunction() {
+    console.log("Running " + count + " time..")
+    // Replace 'https://your-api-endpoint.com' with the actual API endpoint
+    axios.get('https://server-1vb8.onrender.com/pulse')
+      .then(response => {
+        console.log('API Response:', response?.data);
+        // Handle the successful API response here
+      })
+      .catch(error => {
+        console.error('Error calling API:', error);
+        // Handle the error here
+      });
+  }
+  if (intervalId === null) {
+    intervalId = setInterval(() => {
+      repeatFunction();
+      count = count + 1;
+    }, 20000); // Set to run every minute
+  }
+}
+
+// Function to check if the interval is running
+function isIntervalRunning() {
+  return intervalId !== null;
+}
+
+// Function to stop the interval
+// function stopInterval() {
+//   if (intervalId !== null) {
+//     clearInterval(intervalId);
+//     intervalId = null;
+//   }
+// }
+
+// Example usage
+if(!isIntervalRunning()) 
+  startInterval(); // Start the interval
+console.log(isIntervalRunning()); // Check if it's running, should return true// Stop the interval
+console.log(isIntervalRunning()); // Check again, should return false
+
+
+if(isIntervalRunning()) {
+  res.status(200).send({status: "Pulse running "+ count + " times" })
+} else {
+  res.status(200).send({status: "Pulse not running" })
+}
+  
 });
 
 module.exports = router;
