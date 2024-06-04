7af56b6fcd23fa1b01a548ba4c61642d2948eacc
Kevin Paul <b.kevinjoseph@gmail.com>
2024-06-04 11:11:55 +0000

    Added order cron job

--- functions/appFunctions.js
+++ functions/appFunctions.js
@@ -1,55 +1,73 @@
 const Order = require("../models/Order");
 const axios = require("axios");
 
-
 const orderJob = async function checkOrder() {
-    try {
-        // Find orders by userId
-        const userOrders = await Order.find({ paymentStatus: "Processing Payment" });
-        userOrders.map((order) => {
-            const options = {
-                method: "GET",
-                url: `https://sandbox.cashfree.com/pg/orders/${order.orderId}/payments`,
-                headers: {
-                    accept: "application/json",
-                    "x-client-id": "TEST10149202095f693cd23ea188989f20294101",
-                    "x-client-secret":
-                        "cfsk_ma_test_5fdf014335adfec5ea6f0daed5fe727a_d9e59d0c",
-                    "x-api-version": "2022-01-01",
-                },
-            };
-            axios
-                .request(options)
-                .then(async (response) => {
-                    if (response.status === 200) {
-                        if (response?.data?.length > 0) {
-                            if (response?.data[0]?.payment_status === "SUCCESS") {
-                                const existingOrder = await Order.findOne({ orderId: order.orderId });
-                                if (existingOrder) {
-                                    existingOrder.paymentStatus = "Paid";
-                                    await existingOrder.save();
-                                }
-
-                            } else if(response?.data[0]?.payment_status === "FAILED") {
-                                const existingOrder = await Order.findOne({ orderId: order.orderId });
-                                if (existingOrder) {
-                                    existingOrder.paymentStatus = "Failed";
-                                    await existingOrder.save();
-                                }
-                            }
-                        }
-                    } else {
-                        console.log("err");
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+  console.log("Running order cron");
+  try {
+    // Find orders by userId
+    const userOrders = await Order.find({
+      paymentStatus: "Payment Processing",
+    });
+    console.log(userOrders);
+    userOrders.map((order) => {
+      const options = {
+        method: "GET",
+        url: `https://api.cashfree.com/pg/links/${order.orderId}`,
+        headers: {
+          accept: "application/json",
+          // "x-client-id": "TEST10149202095f693cd23ea188989f20294101",
+          "x-client-id": "648837e3016086c32ac96fc7fc738846",
+          // "x-client-secret":
+          //   "cfsk_ma_test_5fdf014335adfec5ea6f0daed5fe727a_d9e59d0c",
+          "x-client-secret":
+            "cfsk_ma_prod_d7af3bd06840368dec08fa8442aa4e0a_48123b3d",
+          "x-api-version": "2023-08-01",
+        },
+      };
+      axios
+        .request(options)
+        .then(async (response) => {
+          if (response.status === 200) {
+            let paymentStatus = response?.data?.link_status;
+            if (paymentStatus === "PAID") {
+              const existingOrder = await Order.findOne({
+                orderId: order.orderId,
+              });
+              if (existingOrder) {
+                existingOrder.paymentStatus = "Paid";
+                console.log("Saving Paid order");
+                await existingOrder.save();
+              }
+            } else if (
+              paymentStatus === "EXPIRED" ||
+              paymentStatus === "TERMINATED" ||
+              paymentStatus === "TERMINATION_REQUESTED"
+            ) {
+              const existingOrder = await Order.findOne({
+                orderId: order.orderId,
+              });
+              if (existingOrder) {
+                existingOrder.paymentStatus = "Failed";
+                console.log("Saving un-Paid order");
+                await existingOrder.save();
+              }
+            }
+            // res.send({
+            //   data: response.data,
+            //   code: 200,
+            // });
+          } else {
+            console.log("err", response);
+          }
         })
-        console.log("Cron ran");
-    } catch (error) {
-        console.log(error);
-    }
-}
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+    console.log("Cron ran");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = orderJob;
